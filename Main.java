2b7d771894d6f15c6c99e281d3af66666d093747
vita792 <59779169+vita792@users.noreply.github.com>
2020-07-03 15:13:56 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         
+        GeometricFigure circle = new Circle("Circle", "red");
+        GeometricFigure triangle = new Triangle("Triangle", "green");
+        GeometricFigure quadrilateral = new Quadrilateral("Quadrilateral", "blue");
+        painter.draw(circle); 
+        painter.draw(triangle); 
+        painter.draw(quadrilateral); 
         Painter.getPainter().drawFigure();
         Painter.getPainter().drawFigure();
         Painter.getPainter().drawFigure();
