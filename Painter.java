94f95b9ac7a671fea001e3851c0c13aaf784a177
vita792 <59779169+vita792@users.noreply.github.com>
2020-07-03 10:10:00 +0300

    Update Painter.java

--- Painter.java
+++ Painter.java
@@ -1,13 +1,15 @@
 package com.company;
 
 class Painter {
-    String id;
-    String FirstName;
-    String LastName;
+    private static Painter id;
+    private static Painter firstName;
+    private static Painter lastName;
 
     void drawFigure(Class<GeometricFigure> drawClass){
     }
+    
  private static final Painter instance = new Painter();
    public static Painter getInstance() {
      return instance;
+}
 }
